7c266e0ca2d3d9d9087cfd2ea293fc726a997b73
Jack_Bob <vialkov.1999@gmail.com>
2026-02-14 16:33:50 +0100

    test fot login

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ class User_Create(BaseModel):
     username: str
     email: EmailStr
     password: str =  Field(min_length=8, max_length=72)
-    phone_number: str | None = None
+    phone_number: str
 
     @field_validator("password")
     @classmethod
@@ -19,13 +19,29 @@ class User_Create(BaseModel):
             raise ValueError("Password must contain at least one lowercase letter")
 
         return value
+    
+    @field_validator("phone_number")
+    @classmethod
+    def validate_phone_number(cls, value: str) -> str:
+        cleaned = value.replace(" ", "").replace("-", "").replace("(", "").replace(")", "")
+
+        digits_only = cleaned.replace("+", "")
+        if not digits_only.isdigit():
+            raise ValueError("Phone number must contain only digits")
+        
+        if len(digits_only) < 10:
+            raise ValueError("Phone number is too short")
+        
+        if len(digits_only) > 15:
+            raise ValueError("Phone number is too long")
+        
+        return cleaned
 
 class User_Out(BaseModel):
     id: int
     username: str
     email: EmailStr
-    phone_number: str | None = None
-
+    phone_number: str 
     class Config:
         from_attributes = True
 
--- tests/auth_test.py
+++ tests/auth_test.py
@@ -35,7 +35,7 @@ def test_register_user_ok():
         "username": "test_user",
         "email": "testuser@example.com",
         "password": "TestPassword123",
-        "phone_number": None,
+        "phone_number": "+48 123 123 123",
     }
 
     response = client.post("/auth/register", json=payload)
@@ -51,14 +51,14 @@ def test_register_duplicate_email():
         "username": "test_user",
         "email": "testuser@example.com",
         "password": "TestPassword123",
-        "phone_number": None,
+        "phone_number": "+48 123 123 123",
     }
 
     payload_2 = {
         "username": "test_user_2",
         "email": "testuser@example.com",
         "password": "TestPassword123",
-        "phone_number": None,
+        "phone_number": "+48 123 123 124",
     }
 
     client.post("/auth/register", json=payload_1)
@@ -71,18 +71,21 @@ def test_register_duplicate_username():
         "username": "test_user",
         "email": "testuser@gmail.com",
         "password": "TestPassword123",
-        "phone_number": None,
+        "phone_number": "+48123123123",
     }
 
     payload_2 = {
         "username": "test_user",
         "email": "testuser_2@gmail.com",
         "password": "TestPassword123",
-        "phone_number": None,
+        "phone_number": "+48123123124",
     }
 
     client.post("/auth/register", json=payload_1)
     response = client.post("/auth/register", json=payload_2)
+
+    if response.status_code != 400:
+        print(response.json())
 
     assert response.status_code == 400
 
@@ -91,7 +94,7 @@ def test_pwd_hasnt_digit():
         "username": "test_user",
         "email": "testuser@gmail.com",
         "password": "Aasdfadfscvacaasd",
-        "phone_number": None,
+        "phone_number": "+48 123 123 123",
     }
 
     response = client.post("/auth/register", json=payload)
@@ -105,7 +108,7 @@ def test_pwd_hasnt_uppercase():
         "username": "test_user",
         "email": "testuser@gmail.com",
         "password": "aasdfadfscvacaasd1",
-        "phone_number": None,
+        "phone_number": "+48 123 123 123",
     }
 
     response = client.post("/auth/register", json=payload)
@@ -119,7 +122,7 @@ def test_pwd_hasnt_lowercase():
         "username": "test_user_0",
         "email": "testuser_0@gmail.com",
         "password": "ASDFASDFASDFASDF1",
-        "phone_number": None,
+        "phone_number": "+48 123 123 123",
     }
 
     response = client.post("/auth/register", json=payload)
@@ -128,3 +131,111 @@ def test_pwd_hasnt_lowercase():
     assert response.status_code == 422
     assert "Password must contain at least one lowercase letter" in data["detail"][0]["msg"]
     
+def test_without_phone_number():
+    payload = {
+        "username": "Jack_bob123",
+        "email": "Jack_bob123@gmail.com",
+        "password": "Jack_bob123",
+    }
+
+    response = client.post("/auth/register", json=payload)
+    data = response.json()
+
+    assert response.status_code == 422
+    assert "Field required" in data["detail"][0]["msg"]
+
+def test_string_number():
+    payload = {
+        "username": "Jack_bob123",
+        "email": "Jack_bob123@gmail.com",
+        "password": "Jack_bob123",
+        "phone_number": "invalid_number"
+    }
+
+    response = client.post("/auth/register", json=payload)
+    data = response.json()
+
+    assert response.status_code == 422
+    assert "Phone number must contain only digits" in data["detail"][0]["msg"]
+
+def test_too_short_number():
+    payload = {
+        "username": "Jack_bob123",
+        "email": "Jack_bob123@gmail.com",
+        "password": "Jack_bob123",
+        "phone_number": "+ 48 123 123"
+    }
+
+    response = client.post("/auth/register", json=payload)
+    data = response.json()
+
+    assert response.status_code == 422
+    assert "Phone number is too short" in data["detail"][0]["msg"]
+
+def test_too_long_number():
+    payload = {
+        "username": "Jack_bob123",
+        "email": "Jack_bob123@gmail.com",
+        "password": "Jack_bob123",
+        "phone_number": "+ 48 123 123 123 123 123 123"
+    }
+
+    response = client.post("auth/register", json=payload)
+    data = response.json()
+
+    assert response.status_code == 422
+    assert "Phone number is too long" in data["detail"][0]["msg"]
+
+def test_login_ok():
+    payload = {
+        "email": "testuser@example.com",
+        "password": "TestPassword123",
+    }
+
+    response = client.post("/auth/login", json=payload)
+    
+    assert response.status_code == 200
+
+def test_login_wrong_password():
+    payload = {
+        "email": "testuser@example.com",
+        "password": "TestPassword1234",
+    }
+
+    response = client.post("/auth/login", json=payload)
+
+    assert response.status_code == 401
+
+def test_login_nonexisting_email():
+    payload = {
+        "email": "testuser____________@example.com",
+        "password": "TestPassword1234",
+    }
+
+    response = client.post("/auth/login", json=payload)
+    data = response.json()
+    if response.status_code != 401:
+        print(data)
+
+    assert response.status_code == 401
+    assert "Invalid email or password" in data["detail"]
+
+def test_login_empty_password():
+    payload = {
+        "email": "testuser@example.com",
+        "password": "",
+    }
+
+    response = client.post("/auth/login", json=payload)
+
+    assert response.status_code == 422
+
+def test_login_invalid_email():
+    payload = {
+        "email": "empty_email",
+        "password": "TestPassword1234",
+    }
+
+    response = client.post("/auth/login", json=payload)
+
+    assert response.status_code == 422
