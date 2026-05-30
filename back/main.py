a702951a2daf9c360621251a4b76c7e1433f8b76
Jack_Bob <vialkov.1999@gmail.com>
2026-05-30 20:31:26 +0200

    fix: add production frontend to CORS

--- back/main.py
+++ back/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:3001",
+        "https://frontend-production-4c7c.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
